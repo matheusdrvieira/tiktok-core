f6dcc98ff283ba75e8293455a5cd644f31346cd6
Matheus Vieira <rochavieira.matheus@gmail.com>
2026-04-23 17:12:12 -0300

    first commit

--- core/src/modules/automation/infra/cron/automation.cron.ts
+++ core/src/modules/automation/infra/cron/automation.cron.ts
@@ -2,12 +2,13 @@ import { cron } from '@elysiajs/cron';
 import { logAndReportError } from '../../../../shared/lib/discord-error';
 import { makeAutomationRunnerService } from '../../application/factory/make-automation-runner-service.factory';
 
-const FIXED_AUTOMATION_CRON_PATTERN = '0 0,6,12,17,21 * * *';
+const FIXED_AUTOMATION_CRON_PATTERN = '0 0,6,12,18,21 * * *';
 const automationRunnerService = makeAutomationRunnerService();
 
 export const automationCron = cron({
   name: 'automation-pipeline',
   pattern: FIXED_AUTOMATION_CRON_PATTERN,
+  timezone: 'America/Sao_Paulo',
   run: async () => {
     try {
       await automationRunnerService.run();
